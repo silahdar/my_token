04a299ce89f6e0feac5e94d1e8799ea55a0a7371
silahdar <amir1b.6@gmail.com>
2022-12-23 17:09:44 +0100

    Set up Rack::Attack

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module App
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
+    config.middleware.use Rack::Attack
+
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
